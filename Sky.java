4a826dd849d43973aaad85e87b78d07bc660a946
DiegoLinares11 <lin221256@uvg.edu.gt>
2023-07-30 22:33:30 -0600

    Counter
    Servirá para dejar inicializado un mapa, por el momento tenemos solo las imágenes de los contadores y del nivel que tenemos que hacerlo funcional.

--- Sky.java
+++ Sky.java
@@ -13,9 +13,25 @@ public class Sky extends World
      * Constructor for objects of class Sky.
      * 
      */
+    private counter score;
+    private counter level;
+    
     public Sky()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 800, 1); 
+        
+        score = new counter("Score: " );
+        level = new counter("Level: " );
+        addObject(score, 140, 90);
+        addObject(level, 140, 60);
+    }
+    public void setUpMap(){
+        Monstruo enemy = new Monstruo();
+        addObject(enemy, 100, 100); // Coloca el objeto "Player" en la posición (100, 100)
+
+        Personaje player = new Personaje();
+        addObject(player, 150, 150); // Coloca el objeto "Enemy" en la posición (700, 500)
+
     }
 }
