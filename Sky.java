3cbd4b49369a22c3d4e006c246346ffebb850768
DiegoLinares11 <lin221256@uvg.edu.gt>
2023-08-06 20:41:22 -0600

    Set primer mapa

--- Sky.java
+++ Sky.java
@@ -16,6 +16,15 @@ public class Sky extends World
     private counter score;
     private counter level;
     
+    private Personaje diego;
+    private Plataforma plat;
+    private Plataforma plat1;
+    private Plataforma plat2;
+    private Plataforma plat3;
+    private Plataforma plat4;
+    
+    private Sup nivel;
+    
     public Sky()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
@@ -23,8 +32,27 @@ public class Sky extends World
         
         score = new counter("Score: " );
         level = new counter("Level: " );
+        diego = new Personaje();
+        plat = new Plataforma();
+        plat1 = new Plataforma();
+        plat2 = new Plataforma();        
+        plat3 = new Plataforma();
+        plat4 = new Plataforma();
+        nivel = new Sup();
+        
+        addObject(diego,100,600);
+        //Plataformas
+        addObject(plat,100,700);
+        addObject(plat1,200,600);
+        addObject(plat2,300,500);
+        addObject(plat3,400,400);
+        addObject(plat4,500,300);
+        addObject(nivel,500,250);
+
         addObject(score, 140, 90);
         addObject(level, 140, 60);
+        
+        
     }
     public void setUpMap(){
         Monstruo enemy = new Monstruo();
